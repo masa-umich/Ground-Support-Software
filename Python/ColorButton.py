02f6e11e250c9404fb78934e6bcf74dfadcc11d8
Alexander Davenport <alexhdavenport@gmail.com>
2020-09-17 12:49:32 -0400

    added color picker buttons and 2 axis plots, non-functional

---
+++ Python/ColorButton.py
@@ -0,0 +1,52 @@
+from PyQt5 import QtGui, QtCore, QtWidgets
+from PyQt5.QtCore import Qt, QRect
+
+class ColorButton(QtWidgets.QPushButton):
+    '''
+    Custom Qt Widget to show a chosen color.
+
+    Left-clicking the button shows the color-chooser, while
+    right-clicking resets the color to None (no-color).    
+    '''
+
+    colorChanged = QtCore.pyqtSignal()
+
+    def __init__(self, *args, **kwargs):
+        super(ColorButton, self).__init__(*args, **kwargs)
+
+        self._color = None
+        self.setMaximumWidth(32)
+        self.pressed.connect(self.onColorPicker)
+
+    def setColor(self, color):
+        if color != self._color:
+            self._color = color
+            self.colorChanged.emit()
+
+        if self._color:
+            self.setStyleSheet("background-color: %s;" % self._color)
+        else:
+            self.setStyleSheet("")
+
+    def color(self):
+        return self._color
+
+    def onColorPicker(self):
+        '''
+        Show color-picker dialog to select color.
+
+        Qt will use the native dialog by default.
+
+        '''
+        dlg = QtWidgets.QColorDialog(self)
+        if self._color:
+            dlg.setCurrentColor(QtGui.QColor(self._color))
+
+        if dlg.exec_():
+            self.setColor(dlg.currentColor().name())
+
+    def mousePressEvent(self, e):
+        if e.button() == Qt.RightButton:
+            self.setColor(None)
+
+        return super(ColorButton, self).mousePressEvent(e)
--- Python/DataViewer.py
+++ Python/DataViewer.py
@@ -3,6 +3,7 @@ from PyQt5 import QtGui, QtCore, QtWidgets
 import pyqtgraph as pg
 import sys
 from Switch import Switch
+from ColorButton import ColorButton
 import pandas as pd
 
 class DataViewer(QtWidgets.QTabWidget):
@@ -12,12 +13,18 @@ class DataViewer(QtWidgets.QTabWidget):
 
     def __init__(self, channels, num_channels=4, *args, **kwargs):
         super(DataViewer, self).__init__(*args, **kwargs)
-
+        
+        # load data channels
         self.channels = channels
 
         # initialize tabs
         self.config_tab = QtWidgets.QWidget()
-        self.plot_tab = pg.GraphicsLayoutWidget()
+        self.plot_tab = QtWidgets.QWidget()
+        self.plot_layout = QtWidgets.QVBoxLayout()
+        self.plot_layout.setContentsMargins(20, 20, 20, 20)
+        self.plot_tab.setLayout(self.plot_layout)
+        self.plot = pg.PlotWidget(background='w')
+        self.plot_layout.addWidget(self.plot)
         self.addTab(self.config_tab, "Config")
         self.addTab(self.plot_tab, "Plot")
 
@@ -27,15 +34,14 @@ class DataViewer(QtWidgets.QTabWidget):
         self.config_tab.setLayout(self.config_layout)
         self.switches = []
         self.series = []
+        self.colors = []
         self.title_edit = QtWidgets.QLineEdit()
         self.title_edit.setPlaceholderText("Plot Title")
-        #self.title_label = QtWidgets.QLabel("Plot Title")
         self.config_layout.addWidget(self.title_edit, 0, 1)
         font = self.title_edit.font()
         font.setPointSize(12)
         self.title_edit.setFont(font)
         self.title_edit.editingFinished.connect(self.titleUpdate)
-        #self.config_layout.addWidget(self.title_label, 0, 0)
         for i in range(num_channels):
             self.switches.append(Switch())
             self.config_layout.addWidget(self.switches[i], i+1, 0)
@@ -46,13 +52,31 @@ class DataViewer(QtWidgets.QTabWidget):
             dropdown.setCompleter(completer)
             self.series.append(dropdown)
             self.config_layout.addWidget(self.series[i], i+1, 1)
+            self.colors.append(ColorButton())
+            self.config_layout.addWidget(self.colors[i], i+1, 2)
+        self.duration_edit = QtWidgets.QLineEdit("30")
+        self.duration_label = QtWidgets.QLabel("Seconds")
+        self.config_layout.addWidget(self.duration_edit, num_channels+1, 0)
+        self.config_layout.addWidget(self.duration_label, num_channels+1, 1)
+        self.duration_edit.editingFinished.connect(self.durationUpdate)
         self.config_layout.setColumnStretch(0, 15)
         self.config_layout.setColumnStretch(1, 85)
+        self.config_layout.setColumnStretch(2, 5)
 
         # setup plot
-        self.plot_tab.setBackground('w')
-        self.plot = pg.PlotItem()
-        self.plot_tab.addItem(self.plot)
+        self.left = self.plot.plotItem
+        self.right = pg.ViewBox()
+        self.left.showAxis('right')
+        self.left.scene().addItem(self.right)
+        self.left.getAxis('right').linkToView(self.right)
+        self.right.setXLink(self.left)
+        self.updateViews()
+        self.left.vb.sigResized.connect(self.updateViews)
+    
+    def updateViews(self):
+        # resize plot on window resize
+        self.right.setGeometry(self.left.vb.sceneBoundingRect())
+        self.right.linkedViewChanged(self.left.vb, self.right.XAxis)
 
     def getActive(self):
         # returns list of active channels
@@ -64,6 +88,9 @@ class DataViewer(QtWidgets.QTabWidget):
 
     def titleUpdate(self):
         self.plot.setTitle(self.title_edit.text())
+    
+    def durationUpdate(self):
+        self.duration = int(self.duration_edit.text)
 
     def update(self, frame):
         pass
