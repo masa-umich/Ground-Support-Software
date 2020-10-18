f4ba48b360423373e907ef20d78160abc24f572f
tybart <tybart@umich.edu>
2020-10-17 22:10:58 -0400

    Feature toolbar

--- Python/GUI.py
+++ Python/GUI.py
@@ -12,8 +12,6 @@ from constants import  Constants
 Program start point. This class handles all child windows of the gui
 """
 class GUI:
-
-
     def __init__(self):
         # Get the screen resolution of the user
         self.screenResolution = [app.desktop().screenGeometry().width(),app.desktop().screenGeometry().height()]
