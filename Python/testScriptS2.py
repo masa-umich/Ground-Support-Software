716460c16d6d2362c19da2659f6d388cc5a72d76
Josh Azrin <jazrin@umich.edu>
2021-01-22 18:34:22 -0500

    this is working, fixed bug when merging with more feature, pre fuckery

--- Python/testScriptS2.py
+++ Python/testScriptS2.py
@@ -1,3 +1,4 @@
+import time
 import serial
 import struct
 import serial.tools.list_ports
@@ -5,22 +6,53 @@ from s2Interface import S2_Interface
 
 s2_interface = S2_Interface()
 ports = s2_interface.scan()
-print(ports)
+#print(ports)
 
 s2_interface.connect("COM4", 115200, 2)
-cmd_info = {
+set_vlv = {
     "function_name": "set_vlv",
     "target_board_addr": 3,
-    "timestamp": 100,
+    "timestamp": 99,
     "args": [1, 1]
 }
-#s2_interface.s2_command(cmd_info)
 
+start = {
+    "function_name": "start_logging",
+    "target_board_addr": 3,
+    "timestamp": 100,
+    "args": []
+}
+
+stop = {
+    "function_name": "stop_logging",
+    "target_board_addr": 3,
+    "timestamp": 101,
+    "args": []
+}
+
+erase = {
+    "function_name" : "wipe_flash",
+    "target_board_addr": 3,
+    "timestamp":102,
+    "args": []
+}
+#s2_interface.s2_command(set_vlv)
+#$time.sleep(1)
+#s2_interface.s2_command(erase)
+#time.sleep(5)
+#s2_interface.s2_command(start)
+#time.sleep(1)
+#s2_interface.s2_command(stop)
+#time.sleep(1)
+s2_interface.download_flash(3, 103)
+
+"""
 while(1):
     parse_status = 0
     parse_status = s2_interface.parse_serial()
     if parse_status == 1:
         print(s2_interface.parser.dict) # get telem values
         print(s2_interface.parser.units) # get units
+"""
 
 
