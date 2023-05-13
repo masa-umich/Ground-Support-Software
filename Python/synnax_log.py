c51a816f89525b42ab6c2e1db624e970c9cdf269
Emiliano Bonilla <ebonilla@synnaxlabs.com>
2023-05-13 07:43:58 -0700

    synnax log update

--- Python/synnax_log.py
+++ Python/synnax_log.py
@@ -112,6 +112,7 @@ class SynnaxLog(io.DataFrameWriter):
 
     def _correct_frame(self, df: DataFrame) -> DataFrame:
         if self._prev_frame is None:
+            self._prev_frame = df
             return df
 
         # Iterate over data in data frame. If the absolute value of any value is greater than 1e5, then set if to
